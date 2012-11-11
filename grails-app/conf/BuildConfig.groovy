8c99a1d254a0e5c87d4d314848e23704f928e8b4
David M. Carr <david@carrclan.us>
2012-11-11 18:28:06 -0500

    mongo-java-driver: update to 0.9.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
             excludes('commons-logging')
         }
         compile('org.reflections:reflections:0.9.8')
-        compile('org.mongodb:mongo-java-driver:2.9.2')
+        compile('org.mongodb:mongo-java-driver:2.9.3')
     }
     plugins {
         build(":tomcat:$grailsVersion",
